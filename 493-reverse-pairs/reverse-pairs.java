2c80d743da04de542a23631321ff7444e3b79f41
Kunal Singhal <149652919+Tr-kunal@users.noreply.github.com>
2026-02-01 15:46:04 +0530

    Time: 77 ms (23.04%) | Memory: 59.1 MB (88.77%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    private void merge(int[] arr, int low, int mid, int high) {
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left++]);
+            } else {
+                temp.add(arr[right++]);
+            }
+        }
+
+        while (left <= mid) temp.add(arr[left++]);
+        while (right <= high) temp.add(arr[right++]);
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    private int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int count = 0;
+
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && arr[i] > 2L * arr[right]) {
+                right++;
+            }
+            count += (right - (mid + 1));
+        }
+        return count;
+    }
+
+    private int mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return 0;
+
+        int mid = low + (high - low) / 2;
+        int count = 0;
+
+        count += mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid + 1, high);
+        count += countPairs(arr, low, mid, high);
+
+        merge(arr, low, mid, high);
+
+        return count;
+    }
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+}

9b1b1ff1702ea81969f7be4b8cfe5311cc04e10c
Kunal Singhal <149652919+Tr-kunal@users.noreply.github.com>
2026-05-02 14:37:54 +0530

    Time: 8 ms (26.02%) | Memory: 41.9 MB (82.58%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,32 +1,25 @@
 class Solution {
     public int rotatedDigits(int n) {
-        int count = 0;
-        
-        for (int i = 1; i <= n; i++) {
-            int num = i;
-            boolean isValid = true;
-            boolean hasChange = false;
-            
-            while (num > 0) {
-                int digit = num % 10;
-                
-                if (digit == 3 || digit == 4 || digit == 7) {
-                    isValid = false;
-                    break;
-                }
-                
-                if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
-                    hasChange = true;
-                }
-                
-                num /= 10;
-            }
-            
-            if (isValid && hasChange) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(isValid(i)){
                 count++;
             }
         }
-        
         return count;
+        
+    }
+    public boolean isValid(int num){
+        boolean valid=false;
+        while(num>0){
+            if(num%10==2||num%10==5||num%10==6||num%10==9){
+                valid=true;
+            }
+            if(num%10==3||num%10==4||num%10==7){
+                return false;
+            }
+            num/=10;
+        }
+        return valid;
     }
 }
